43100522be8b43187751560b0bf0fcf151640910
nillehcar <lin00104@algonquinlive.com>
2014-11-13 09:11:23 -0500

    added new js scroll

--- js/slider.js
+++ js/slider.js
@@ -65,41 +65,9 @@
    
 });   
 
-$("a[href='#top']").click(function() {
-$("html, body").animate({ scrollTop: 0 }, "slow");
-return false;
-}); 
-    
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1200);
-            return false;
-        }
-    }
-});
-    
-    
-$(function() {
-$('a[href*=#]:not([href=#])').click(function() {
-if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-var target = $(this.hash);
-target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-if (target.length) {
-$('html,body').animate({
-  scrollTop: target.offset().top
-}, 1000);
-return false;
-}
-}
-});
-});
+$('.nav-list').localScroll();
+$('.grid').localScroll();
+$('#footer').localScroll();
 
   $(function() {
     $(".rslides").responsiveSlides();
